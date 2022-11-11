0e97989f3edd08eed1c1919551f37f8aa66f81d1
yunyiwinnieshe <pineapplesyy@gmail.com>
2022-11-10 16:05:07 -0800

    part 1 finish

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,10 +68,14 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  if(window.localStorage.getItem('recipes')){
+    return JSON.parse(window.localStorage.getItem('recipes'));
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipeArray = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
@@ -82,7 +86,7 @@ async function getRecipes() {
   // we're returning
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
+  // declared above
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
@@ -100,6 +104,27 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  
+  return new Promise(async(resolve, reject) => {
+    for(var i = 0; i < RECIPE_URLS.length; i++){
+      try{
+        const response = await fetch(RECIPE_URLS[i]);
+        const data = await response.json();
+        recipeArray.append(data);
+        if(recipeArray.length == RECIPE_URLS.length){
+          saveRecipesToStorage(recipeArray);
+          resolve(recipeArray);
+        }
+        
+      }
+      catch(error){
+        console.error("There's a mistake")
+        reject(error);
+      }
+        
+    };
+
+  });
 }
 
 /**
