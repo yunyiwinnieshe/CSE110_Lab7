5cfb7f9ce2f69aab3454da3e4cd64224865632e6
Elliot Alexander Lee <eal001@ucsd.edu>
2022-11-13 12:42:14 -0800

    implement service worker

--- sw.js
+++ sw.js
@@ -3,13 +3,23 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+// CONSTANTS
+const RECIPE_URLS = [
+  'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+  'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+  'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+  'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+];
+
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
+      // B6. - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -32,9 +42,29 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
+  // B7. - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  caches.open(CACHE_NAME).then((cache) => {
+    // B8. - If the request is in the cache, return with the cached version.
+    //            Otherwise fetch the resource, add it to the cache, and return
+    //            network response.
+
+    // handle returning a valid response
+    return cache.match(event.request.url).then( (cache_response) => {
+      // first try the cached response
+      if(cache_response){
+        return cache_response;
+      } 
+      // otherwise fetch the resource from network
+      else {
+        // call fetch request
+        fetch(event.request).then( (network_response) => {
+          // store new request response pair to cache
+          cache.put(event.request, network_response);
+          // return response
+          return network_response; 
+        });
+      }
+    });
+  });
 });
